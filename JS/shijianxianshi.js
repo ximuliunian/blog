df165eaa75a80a3d018840badfdee542216e8057
曦暮流年 <98019029+ximuliunian@users.noreply.github.com>
2022-08-02 18:34:01 +0800

    2022-8-2

--- JS/shijianxianshi.js
+++ JS/shijianxianshi.js
@@ -11,12 +11,14 @@ function momxc() {
     var timeold = today.getTime() - birthDay.getTime();
     var sectimeold = timeold / 1000
     var secondsold = Math.floor(sectimeold);
-    var msPerDay = 24 * 60 * 60 * 1000; var e_daysold = timeold / msPerDay;
+    var msPerDay = 24 * 60 * 60 * 1000;
+    var e_daysold = timeold / msPerDay;
     var daysold = Math.floor(e_daysold);
     var e_hrsold = (daysold - e_daysold) * -24;
     var hrsold = Math.floor(e_hrsold);
     var e_minsold = (hrsold - e_hrsold) * -60;
-    var minsold = Math.floor((hrsold - e_hrsold) * -60); var seconds = Math.floor((minsold - e_minsold) * -60).toString();
-    document.getElementById("momk").innerHTML = "运行" + daysold + "天" + hrsold + "小时" + minsold + "分" + seconds + "秒";
+    var minsold = Math.floor((hrsold - e_hrsold) * -60);
+    var seconds = Math.floor((minsold - e_minsold) * -60).toString();
+    document.getElementById("momk").innerHTML = "" + daysold + "天" + hrsold + "小时" + minsold + "分" + seconds + "秒";
     setTimeout(momxc, 1000);
 } momxc();
